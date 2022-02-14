1596855518c6f0c97add12e77eba33d91cc03209
crazyyanchao <crazyyanchao@users.noreply.github.com>
2022-02-14 12:33:08 +0000

    Automated deployment: Mon Feb 14 12:33:08 UTC 2022 85daca0af31626eee337821f106b043f55000feb

---
+++ precache-manifest.48e1fa7cdecb2f12ad6e01fc5c29dae1.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59e221032ab061cad83b6ce2bcddbde8",
+    "url": "/preact-github-actions-to-github-pages/assets/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "cf3fdf7af60a294d6d3f48cb7ad82488",
+    "url": "/preact-github-actions-to-github-pages/assets/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "a0e46feb3cc577478b127936e739dd08",
+    "url": "/preact-github-actions-to-github-pages/assets/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "d712b605ed58419c7e6d4ab885d147b7",
+    "url": "/preact-github-actions-to-github-pages/assets/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "2f7ce797cf8f198dedb9a9f38b7ef13b",
+    "url": "/preact-github-actions-to-github-pages/assets/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "ba817517b2c4e1ba1ce802c4d4fafdb4",
+    "url": "/preact-github-actions-to-github-pages/assets/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "ae3804f6eab4d46718e5",
+    "url": "/preact-github-actions-to-github-pages/bundle.7e56a.css"
+  },
+  {
+    "revision": "ae3804f6eab4d46718e5",
+    "url": "/preact-github-actions-to-github-pages/bundle.ae380.js"
+  },
+  {
+    "revision": "cc0bf6384004ebb38929977c7284a742",
+    "url": "/preact-github-actions-to-github-pages/index.html"
+  },
+  {
+    "revision": "e58efe8212d15cc4441c",
+    "url": "/preact-github-actions-to-github-pages/polyfills.e58ef.js"
+  },
+  {
+    "revision": "778d81bae98cd026d90a",
+    "url": "/preact-github-actions-to-github-pages/route-home.chunk.778d8.js"
+  },
+  {
+    "revision": "778d81bae98cd026d90a",
+    "url": "/preact-github-actions-to-github-pages/route-home.chunk.e6c71.css"
+  },
+  {
+    "revision": "af7281e94c99428477ad",
+    "url": "/preact-github-actions-to-github-pages/route-profile.chunk.62c75.css"
+  },
+  {
+    "revision": "af7281e94c99428477ad",
+    "url": "/preact-github-actions-to-github-pages/route-profile.chunk.af728.js"
+  },
+  {
+    "revision": "13b82ed03d648451c57be206f75f0216",
+    "url": "/preact-github-actions-to-github-pages/sw-debug.js"
+  },
+  {
+    "revision": "15969c26ddbab93fd045a21329f6237d",
+    "url": "/preact-github-actions-to-github-pages/sw-esm.js"
+  },
+  {
+    "revision": "d9837c3312c0119088569214e2e84c86",
+    "url": "/preact-github-actions-to-github-pages/sw.js"
+  }
+]);
---
+++ precache-manifest.f4dd11bcf1b3bd299273e3e1946da788.esm.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59e221032ab061cad83b6ce2bcddbde8",
+    "url": "/preact-github-actions-to-github-pages/assets/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "cf3fdf7af60a294d6d3f48cb7ad82488",
+    "url": "/preact-github-actions-to-github-pages/assets/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "a0e46feb3cc577478b127936e739dd08",
+    "url": "/preact-github-actions-to-github-pages/assets/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "d712b605ed58419c7e6d4ab885d147b7",
+    "url": "/preact-github-actions-to-github-pages/assets/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "2f7ce797cf8f198dedb9a9f38b7ef13b",
+    "url": "/preact-github-actions-to-github-pages/assets/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "ba817517b2c4e1ba1ce802c4d4fafdb4",
+    "url": "/preact-github-actions-to-github-pages/assets/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "b23cda95230c91a749d7178e18085e16",
+    "url": "/preact-github-actions-to-github-pages/bundle.2a7e7.esm.js"
+  },
+  {
+    "revision": "ae3804f6eab4d46718e5",
+    "url": "/preact-github-actions-to-github-pages/bundle.7e56a.css"
+  },
+  {
+    "revision": "cc0bf6384004ebb38929977c7284a742",
+    "url": "/preact-github-actions-to-github-pages/index.html"
+  },
+  {
+    "revision": "5e428982a7422613c323204f130d9126",
+    "url": "/preact-github-actions-to-github-pages/polyfills.749dd.esm.js"
+  },
+  {
+    "revision": "dc573848d92185eaf6910c628ac17070",
+    "url": "/preact-github-actions-to-github-pages/route-home.chunk.60b96.esm.js"
+  },
+  {
+    "revision": "778d81bae98cd026d90a",
+    "url": "/preact-github-actions-to-github-pages/route-home.chunk.e6c71.css"
+  },
+  {
+    "revision": "2ed7830b82908ef0783d59413f0d41b4",
+    "url": "/preact-github-actions-to-github-pages/route-profile.chunk.38290.esm.js"
+  },
+  {
+    "revision": "af7281e94c99428477ad",
+    "url": "/preact-github-actions-to-github-pages/route-profile.chunk.62c75.css"
+  }
+]);
